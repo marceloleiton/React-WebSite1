6b22baf212533393e4fafea2457b4c76c8c74af9
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-11-15 13:02:25 -0300

    Navs menu created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,29 +1,49 @@
 //rfce para crear automatico importar react funcion
 import React, {useState} from 'react'
-import { FaTimes } from 'react-icons/fa'
-import { FaBars} from 'react-icons/fa'
-import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon} from './Navbar.elements'
+import { FaTimes, FaBars } from 'react-icons/fa'
 import { IconContext} from 'react-icons/lib'
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    NavIcon,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+} from './Navbar.elements'
+
 const Navbar = () => {
     const [click, setClick] = useState(false)
     const handleClick = () => setClick(!click)
     return (
         <>
-        <IconContext.Provider value={{color: '#fff'}}>
-            <Nav>
-               <NavbarContainer>
-                    <NavLogo to='/'>
-                        <NavIcon/>
-                       LACASADELAMOR
-                    </NavLogo>
-                    <MobileIcon onClick={handleClick}>
-                       {click ? <FaTimes /> : <FaBars/>}
-                    </MobileIcon>
-               </NavbarContainer>
-            </Nav>
-        </IconContext.Provider> 
-    </>
-    )
-}
+            <IconContext.Provider value={{color: '#fff'}}>
+                <Nav>
+                <NavbarContainer>
+                        <NavLogo to='/'>
+                            <NavIcon/>
+                        LACASADELAMOR
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars/>}
+                        </MobileIcon>
+            //NavMenu es la que contiene los navegadores de la página y la redirección        
+                        <NavMenu onClick={handleClick}>
+                            <NavItem>
+                                <NavLinks to='/'>inicio</NavLinks>
+                            </NavItem>
+                        </NavMenu>
+                        <NavMenu onClick={handleClick}>
+                            <NavItem>
+                                <NavLinks to='
+                                /nosotros'>nosotros</NavLinks>
+                            </NavItem>
+                        </NavMenu>
+                </NavbarContainer>
+                </Nav>
+            </IconContext.Provider> 
+        </>
+    )};
 
 export default Navbar
