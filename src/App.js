d80cb25920c0feed02c3edd90f5749e1bbe378de
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-11-15 16:30:33 -0300

    Section with info in basics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Navbar } from './components';
 import GlobalStyles from './globalStyles';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/HomePage/Home'
 function App() {
   return (
     <Router>
       <GlobalStyles/> 
-     <Navbar/>
+     <Navbar/> 
+     <Switch>
+       <Route path="/" exact component={Home}/>
+     </Switch>
     </Router>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,2 @@
 export { default as Navbar } from './Navbar/Navbar';
-export { default as InfoSection } from './InfoSecction/InfoSecction';
+export { default as InfoSection } from './InfoSection/Inf oSection';
