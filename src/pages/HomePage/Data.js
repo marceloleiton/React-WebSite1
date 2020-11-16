b0459e0f88a5f5eddb2a4f02ac5976088f75b2af
Marcelo Leiton <marcelo.leiton@alumnos.uv.cl>
2020-11-16 02:15:30 -0300

    test

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,15 +1,15 @@
 export const homeObjOne={
     lightBg:false,
     primary:true, 
-    imgStart:'',
+    imgStart: '',
     lightTopLine:true,
     lightTextDesc:true,
-    buttonLabel: 'contactanos',
+    buttonLabel: 'contactanos', 
     description:'tiene como finalidad presentar una propuesta de valores humanos universales de integración cultural para ayudar a la transformación del hombre mediante el servicio a los demás y la autodesignación que le conducza al conocimiento de su verdadera realizad.',
     headLine:'Calli de Tlasothlalistli (Casa del Amor) A.C',
     lightText:true, 
     topLine:'Oganización no gubernamental (ONG)',
-    img:require('../../Images/'),
+    img: require('../../images'),
     alt:'Image',
     start:''
 };
